edc1f9d34a23b9ee4dcec1f57dc1130a41bf5a16
aurorax-neo <15047150695@163.com>
2024-12-06 16:24:45 +0800

    feat(example): Add network server logging and lifecycle functions
    新增网络服务器日志记录和生命周期函数，以便更好地监控和管理网络服务器的状态。  `start`, `early_update`, `update`, `late_update`, `on_destroy` 函数被添加到 `examples/mirror.rs`  文件中，用于处理服务器的启动、更新和销毁过程，并记录相关信息。  注释掉的代码块保留了用于将来扩展的可能性。

--- examples/mirror.rs
+++ examples/mirror.rs
@@ -1,4 +1,5 @@
 use crate::quick_start::player_script::PlayerScript;
+use Mirror_rust::log_debug;
 use Mirror_rust::mirror::authenticators::basic_authenticator::BasicAuthenticator;
 use Mirror_rust::mirror::authenticators::network_authenticator::NetworkAuthenticatorTrait;
 use Mirror_rust::mirror::core::backend_data::NetworkBehaviourComponent;
@@ -7,6 +8,7 @@ use Mirror_rust::mirror::core::network_behaviour::{
 };
 use Mirror_rust::mirror::core::network_loop::NetworkLoop;
 use Mirror_rust::mirror::core::network_manager::NetworkManagerStatic;
+use Mirror_rust::mirror::core::network_server::NetworkServerStatic;
 use Mirror_rust::mirror::core::network_start_position::NetworkStartPosition;
 use Mirror_rust::mirror::core::transport::TransportTrait;
 use Mirror_rust::mirror::transports::kcp2k::kcp2k_transport::Kcp2kTransport;
@@ -33,10 +35,36 @@ fn on_enable() {
     BasicAuthenticator::new("123".to_string(), "456".to_string()).enable();
 }
 
+fn start() {
+    NetworkServerStatic::for_each_network_message_handler(|item| {
+        log_debug!(format!(
+            "message hash: {} require_authentication: {}",
+            item.key(),
+            item.require_authentication
+        ));
+    });
+}
+
+fn early_update() {}
+
+fn update() {}
+
+fn late_update() {
+    // NetworkServerStatic::for_each_network_connection(|item| {
+    //     log_debug!(format!(
+    //         "connection hash: {} address: {}",
+    //         item.key(),
+    //         item.address
+    //     ));
+    // });
+}
+
 fn on_disable() {
     // 禁用基础认证
     NetworkManagerStatic::get_network_manager_singleton().dis_enable_authenticator();
 }
+
+fn on_destroy() {}
 
 fn main() {
     // 添加网络行为工厂
@@ -45,8 +73,18 @@ fn main() {
     NetworkLoop::add_awake_function(awake);
     // 添加 on_enable 函数
     NetworkLoop::add_on_enable_function(on_enable);
+    // 添加 start 函数
+    NetworkLoop::add_start_function(start);
+    // 添加 early_update 函数
+    NetworkLoop::add_early_update_function(early_update);
+    // 添加 update 函数
+    NetworkLoop::add_update_function(update);
+    // 添加 late_update 函数
+    NetworkLoop::add_late_update_function(late_update);
     // 添加 on_disable 函数
     NetworkLoop::add_on_disable_function(on_disable);
+    // 添加 on_destroy 函数
+    NetworkLoop::add_on_destroy_function(on_destroy);
     // NetworkLoop
     NetworkLoop::run();
 }
