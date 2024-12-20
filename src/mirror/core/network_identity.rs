8f7ae6772a47fdfba432fedaa0114a65b529e1c7
aurorax-neo <15047150695@163.com>
2024-12-20 18:06:48 +0800

    refactor(mirror): Remove unnecessary awake calls
    Remove redundant calls to `awake()` in `NetworkIdentity`
    constructor methods.  This simplifies the code and improves
    readability without altering functionality.

--- src/mirror/core/network_identity.rs
+++ src/mirror/core/network_identity.rs
@@ -84,7 +84,6 @@ impl NetworkIdentity {
     pub fn new_with_asset_id(asset_id: u32) -> Self {
         let mut network_identity = Self::new();
         network_identity.asset_id = asset_id;
-        network_identity.awake();
         network_identity
     }
     pub fn new_with_scene_id(scene_id: u64) -> Self {
@@ -95,7 +94,6 @@ impl NetworkIdentity {
         {
             network_identity.valid_parent = temp.valid_parent;
         }
-        network_identity.awake();
         network_identity
     }
     pub fn new() -> Self {
