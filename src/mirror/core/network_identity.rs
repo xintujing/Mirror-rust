269768a3a13318d3a6231c84a36a222f019d3b25
aurorax-neo <15047150695@163.com>
2025-01-07 16:43:41 +0800

    refactor: Improve network identity component handling
    Use a counter and map for managing network behaviours,
    instead of a vector. Improve component access and
    serialization.

--- src/mirror/core/network_identity.rs
+++ src/mirror/core/network_identity.rs
@@ -1,13 +1,12 @@
 use crate::log_error;
 use crate::mirror::core::backend_data::BackendDataStatic;
 use crate::mirror::core::network_behaviour::{
-    GameObject, NetworkBehaviourFactory, NetworkBehaviourTrait, SyncDirection, SyncMode,
+    GameObject, NetworkBehaviourFactory, SyncDirection, SyncMode,
 };
 use crate::mirror::core::network_connection::NetworkConnectionTrait;
 use crate::mirror::core::network_connection_to_client::NetworkConnectionToClient;
 use crate::mirror::core::network_reader::{NetworkReader, NetworkReaderTrait};
-use crate::mirror::core::network_reader_pool::NetworkReaderPool;
-use crate::mirror::core::network_server::{NetworkServer, NetworkServerStatic};
+use crate::mirror::core::network_server::{NetworkServer, NetworkServerStatic, NETWORK_BEHAVIOURS};
 use crate::mirror::core::network_writer::{NetworkWriter, NetworkWriterTrait};
 use crate::mirror::core::network_writer_pool::NetworkWriterPool;
 use crate::mirror::core::remote_calls::{RemoteCallType, RemoteProcedureCalls};
@@ -76,7 +75,7 @@ pub struct NetworkIdentity {
     pub scene_ids: DashMap<u64, u32>,
     pub has_spawned: bool,
     pub spawned_from_instantiate: bool,
-    pub network_behaviours: Vec<Box<dyn NetworkBehaviourTrait>>,
+    pub network_behaviours_count: u8,
 }
 
 impl NetworkIdentity {
@@ -111,19 +110,21 @@ impl NetworkIdentity {
             scene_ids: Default::default(),
             has_spawned: false,
             spawned_from_instantiate: false,
-            network_behaviours: Default::default(),
+            network_behaviours_count: 0,
         }
     }
     pub fn net_id(&self) -> u32 {
         self.net_id
     }
     pub fn set_net_id(&mut self, net_id: u32) {
+        // 设置所有的 component 的 net_id
+        NETWORK_BEHAVIOURS::update_behaviour_net_id(
+            self.net_id,
+            net_id,
+            self.network_behaviours_count,
+        );
         // 设置 net_id
         self.net_id = net_id;
-        // 设置所有的 component 的 net_id
-        for component in self.network_behaviours.iter_mut() {
-            component.set_net_id(self.net_id);
-        }
         // 如果 conn_to_client 不为0，设置 connection_to_client 的 net_id
         if self.conn_to_client == 0 {
             return;
@@ -149,19 +150,21 @@ impl NetworkIdentity {
         self.net_id == 0
             && self.asset_id == 0
             && self.game_object.is_null()
-            && self.network_behaviours.len() == 0
+            && self.network_behaviours_count == 0
             && self.scene_id == 0
     }
     pub fn connection_to_client(&self) -> u64 {
         self.conn_to_client
     }
     pub fn set_connection_to_client(&mut self, conn_id: u64) {
+        // 设置所有的component的conn_id
+        NETWORK_BEHAVIOURS::update_behaviour_conn_id(
+            self.net_id,
+            conn_id,
+            self.network_behaviours_count,
+        );
         // 设置 conn_id
         self.conn_to_client = conn_id;
-        // 设置所有的component的conn_id
-        for component in self.network_behaviours.iter_mut() {
-            component.set_connection_to_client(self.conn_to_client);
-        }
         // 如果 conn_to_client 不为0，设置 connection_to_client 的 net_id
         if self.conn_to_client == 0 {
             return;
@@ -184,32 +187,37 @@ impl NetworkIdentity {
     }
     pub fn set_game_object(&mut self, game_object: GameObject) {
         self.game_object = game_object;
-        for component in self.network_behaviours.iter_mut() {
-            component.set_game_object(self.game_object.clone());
+        for i in 0..self.network_behaviours_count {
+            match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", self.net_id, i)) {
+                TryResult::Present(mut component) => {
+                    component.set_game_object(self.game_object.clone());
+                }
+                TryResult::Absent => {
+                    log_error!("Failed to set game object because component is absent.");
+                }
+                TryResult::Locked => {
+                    log_error!("Failed to set game object because component is locked.");
+                }
+            }
         }
     }
 
     pub fn handle_remote_call(
-        &mut self,
+        conn_id: u64,
+        net_id: u32,
         component_index: u8,
         function_hash: u16,
-        remote_call_type: RemoteCallType,
         reader: &mut NetworkReader,
-        conn_id: u64,
+        remote_call_type: RemoteCallType,
     ) {
-        if component_index as usize >= self.network_behaviours.len() {
-            log_error!("Component index out of bounds: ", component_index);
-            return;
-        }
-
         // 调用 invoke
         if !RemoteProcedureCalls::invoke(
-            function_hash,
-            remote_call_type,
-            self,
-            component_index,
-            reader,
             conn_id,
+            net_id,
+            component_index,
+            function_hash,
+            reader,
+            remote_call_type,
         ) {
             log_error!(
                 "Failed to invoke remote call for function hash: ",
@@ -234,11 +242,19 @@ impl NetworkIdentity {
             for component in BackendDataStatic::get_backend_data()
                 .get_network_identity_data_network_behaviour_components_by_asset_id(self.asset_id)
             {
-                if let Some(network_behaviour) = NetworkBehaviourFactory::create_network_behaviour(
-                    self.game_object.clone(),
-                    &component,
-                ) {
-                    self.network_behaviours.push(network_behaviour);
+                if let Some(mut network_behaviour) =
+                    NetworkBehaviourFactory::create_network_behaviour(
+                        self.game_object.clone(),
+                        &component,
+                    )
+                {
+                    network_behaviour.set_net_id(self.net_id);
+                    NETWORK_BEHAVIOURS::add_behaviour(
+                        self.net_id,
+                        network_behaviour.index(),
+                        network_behaviour,
+                    );
+                    self.network_behaviours_count += 1;
                 }
             }
         }
@@ -246,11 +262,19 @@ impl NetworkIdentity {
             for component in BackendDataStatic::get_backend_data()
                 .get_network_identity_data_network_behaviour_components_by_scene_id(self.scene_id)
             {
-                if let Some(network_behaviour) = NetworkBehaviourFactory::create_network_behaviour(
-                    self.game_object.clone(),
-                    &component,
-                ) {
-                    self.network_behaviours.push(network_behaviour);
+                if let Some(mut network_behaviour) =
+                    NetworkBehaviourFactory::create_network_behaviour(
+                        self.game_object.clone(),
+                        &component,
+                    )
+                {
+                    network_behaviour.set_net_id(self.net_id);
+                    NETWORK_BEHAVIOURS::add_behaviour(
+                        self.net_id,
+                        network_behaviour.index(),
+                        network_behaviour,
+                    );
+                    self.network_behaviours_count += 1;
                 }
             }
         }
@@ -277,36 +301,66 @@ impl NetworkIdentity {
         }
     }
     pub fn validate_components(&self) {
-        if self.network_behaviours.len() > 64 {
+        if self.network_behaviours_count > 64 {
             log_error!("NetworkIdentity has too many components. Max is 64.");
         }
     }
     pub fn on_start_server(&mut self) {
-        self.network_behaviours
-            .iter_mut()
-            .for_each(|component| component.on_start_server());
+        for i in 0..self.network_behaviours_count {
+            match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", self.net_id, i)) {
+                TryResult::Present(mut component) => {
+                    component.on_start_server();
+                }
+                TryResult::Absent => {
+                    log_error!("Failed to start server because component is absent.");
+                }
+                TryResult::Locked => {
+                    log_error!("Failed to start server because component is locked.");
+                }
+            }
+        }
     }
     pub fn on_stop_server(&mut self) {
-        self.network_behaviours
-            .iter_mut()
-            .for_each(|component| component.on_stop_server());
+        for i in 0..self.network_behaviours_count {
+            match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", self.net_id, i)) {
+                TryResult::Present(mut component) => {
+                    component.on_stop_server();
+                }
+                TryResult::Absent => {
+                    log_error!("Failed to stop server because component is absent.");
+                }
+                TryResult::Locked => {
+                    log_error!("Failed to stop server because component is locked.");
+                }
+            }
+        }
     }
     fn server_dirty_masks(&mut self, initial_state: bool) -> (u64, u64) {
         let mut owner_mask: u64 = 0;
         let mut observers_mask: u64 = 0;
-        for (i, component) in self.network_behaviours.iter_mut().enumerate() {
-            let nth_bit = 1 << i;
-            let dirty = component.is_dirty();
+        for i in 0..self.network_behaviours_count {
+            match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", self.net_id, i)) {
+                TryResult::Present(mut component) => {
+                    let nth_bit = 1 << i;
+                    let dirty = component.is_dirty();
 
-            if initial_state
-                || (*component.sync_direction() == SyncDirection::ServerToClient) && dirty
-            {
-                owner_mask |= nth_bit;
-            }
+                    if initial_state
+                        || (*component.sync_direction() == SyncDirection::ServerToClient) && dirty
+                    {
+                        owner_mask |= nth_bit;
+                    }
 
-            if *component.sync_mode() == SyncMode::Observers {
-                if initial_state || dirty {
-                    observers_mask |= nth_bit;
+                    if *component.sync_mode() == SyncMode::Observers {
+                        if initial_state || dirty {
+                            observers_mask |= nth_bit;
+                        }
+                    }
+                }
+                TryResult::Absent => {
+                    log_error!("Failed to get server dirty masks because component is absent.");
+                }
+                TryResult::Locked => {
+                    log_error!("Failed to get server dirty masks because component is locked.");
                 }
             }
         }
@@ -332,26 +386,36 @@ impl NetworkIdentity {
         }
 
         if (owner_mask | observers_mask) != 0 {
-            for (i, component) in self.network_behaviours.iter_mut().enumerate() {
-                let owner_dirty = Self::is_dirty(owner_mask, i as u8);
-                let observers_dirty = Self::is_dirty(observers_mask, i as u8);
+            for i in 0..self.network_behaviours_count {
+                match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", self.net_id, i)) {
+                    TryResult::Present(mut component) => {
+                        let owner_dirty = Self::is_dirty(owner_mask, i);
+                        let observers_dirty = Self::is_dirty(observers_mask, i);
 
-                if owner_dirty || observers_dirty {
-                    NetworkWriterPool::get_return(|temp| {
-                        // Serialize the component
-                        component.serialize(temp, initial_state);
+                        if owner_dirty || observers_dirty {
+                            NetworkWriterPool::get_return(|temp| {
+                                // Serialize the component
+                                component.serialize(temp, initial_state);
 
-                        let segment = temp.to_bytes();
+                                let segment = temp.to_bytes();
 
-                        if owner_dirty {
-                            owner_writer.write_array_segment_all(&segment);
+                                if owner_dirty {
+                                    owner_writer.write_array_segment_all(&segment);
+                                }
+                                if observers_dirty {
+                                    observers_writer.write_array_segment_all(&segment);
+                                }
+                            });
+                            if !initial_state {
+                                component.clear_all_dirty_bits();
+                            }
                         }
-                        if observers_dirty {
-                            observers_writer.write_array_segment_all(&segment);
-                        }
-                    });
-                    if !initial_state {
-                        component.clear_all_dirty_bits();
+                    }
+                    TryResult::Absent => {
+                        log_error!("Failed to serialize server because component is absent.");
+                    }
+                    TryResult::Locked => {
+                        log_error!("Failed to serialize server because component is locked.");
                     }
                 }
             }
@@ -362,15 +426,23 @@ impl NetworkIdentity {
 
         let mask = reader.decompress_var_ulong();
 
-        for (i, component) in self.network_behaviours.iter_mut().enumerate() {
-            if Self::is_dirty(mask, i as u8) {
-                if *component.sync_direction() == SyncDirection::ClientToServer {
-                    NetworkReaderPool::get_return(|reader| {
-                        if !component.deserialize(reader, false) {
-                            return;
+        for i in 0..self.network_behaviours_count {
+            if Self::is_dirty(mask, i) {
+                match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", self.net_id, i)) {
+                    TryResult::Present(mut component) => {
+                        if component.sync_direction() == &SyncDirection::ServerToClient {
+                            if !component.deserialize(reader, false) {
+                                return false;
+                            }
+                            component.set_dirty();
                         }
-                        component.set_dirty();
-                    });
+                    }
+                    TryResult::Absent => {
+                        log_error!("Failed to deserialize server because component is absent.");
+                    }
+                    TryResult::Locked => {
+                        log_error!("Failed to deserialize server because component is locked.");
+                    }
                 }
             }
         }
@@ -464,8 +536,22 @@ impl NetworkIdentity {
         }
     }
     fn clear_all_components_dirty_bits(&mut self) {
-        for component in self.network_behaviours.iter_mut() {
-            component.clear_all_dirty_bits()
+        for i in 0..self.network_behaviours_count {
+            match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", self.net_id, i)) {
+                TryResult::Present(mut component) => {
+                    component.clear_all_dirty_bits();
+                }
+                TryResult::Absent => {
+                    log_error!(
+                        "Failed to clear all components dirty bits because component is absent."
+                    );
+                }
+                TryResult::Locked => {
+                    log_error!(
+                        "Failed to clear all components dirty bits because component is locked."
+                    );
+                }
+            }
         }
     }
     pub fn remove_observer(&mut self, conn_id: u64) {
@@ -491,44 +577,44 @@ impl NetworkIdentity {
         self.game_object.set_active(active);
     }
 
-    pub fn get_component<T>(&mut self) -> Option<&mut T>
-    where
-        T: NetworkBehaviourTrait,
-    {
-        for component in self.network_behaviours.iter_mut() {
-            if let Some(component) = component.as_any_mut().downcast_mut::<T>() {
-                return Some(component);
-            }
-        }
-        None
-    }
-
-    pub fn get_component_by_index(
-        &mut self,
-        index: u8,
-    ) -> Option<&mut Box<dyn NetworkBehaviourTrait>> {
-        if index as usize >= self.network_behaviours.len() {
-            return None;
-        }
-        for component in self.network_behaviours.iter_mut() {
-            if index == component.index() {
-                return Some(component);
-            }
-        }
-        None
-    }
-
-    pub fn get_component_by_sub_class(
-        &mut self,
-        sub_class: &str,
-    ) -> Option<&mut Box<dyn NetworkBehaviourTrait>> {
-        for component in self.network_behaviours.iter_mut() {
-            if sub_class == component.sub_class() {
-                return Some(component);
-            }
-        }
-        None
-    }
+    // pub fn get_component<T>(&mut self) -> Option<&mut T>
+    // where
+    //     T: NetworkBehaviourTrait,
+    // {
+    //     for component in self.network_behaviours.iter_mut() {
+    //         if let Some(component) = component.as_any_mut().downcast_mut::<T>() {
+    //             return Some(component);
+    //         }
+    //     }
+    //     None
+    // }
+    //
+    // pub fn get_component_by_index(
+    //     &mut self,
+    //     index: u8,
+    // ) -> Option<&mut Box<dyn NetworkBehaviourTrait>> {
+    //     if index as usize >= self.network_behaviours.len() {
+    //         return None;
+    //     }
+    //     for component in self.network_behaviours.iter_mut() {
+    //         if index == component.index() {
+    //             return Some(component);
+    //         }
+    //     }
+    //     None
+    // }
+    //
+    // pub fn get_component_by_sub_class(
+    //     &mut self,
+    //     sub_class: &str,
+    // ) -> Option<&mut Box<dyn NetworkBehaviourTrait>> {
+    //     for component in self.network_behaviours.iter_mut() {
+    //         if sub_class == component.sub_class() {
+    //             return Some(component);
+    //         }
+    //     }
+    //     None
+    // }
 
     pub fn remove_client_authority(&mut self) {
         if self.conn_to_client == 0 {
