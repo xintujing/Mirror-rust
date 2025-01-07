6aa32de184abaa485ac7691539f6454d70855b28
aurorax-neo <15047150695@163.com>
2025-01-07 16:44:09 +0800

    fix: Correctly invoke user code commands on server
    Fixes how `invoke_user_code_cmd` calls user code. It now fetches the
    NetworkBehaviour from a static store instead of relying on an ID,
    and also passes the correct conn_id. Also add locking protection.

--- examples/quick_start/player_script.rs
+++ examples/quick_start/player_script.rs
@@ -1,9 +1,10 @@
+use dashmap::try_result::TryResult;
 use mirror_rust::log_error;
 use mirror_rust::mirror::components::network_common_behaviour::NetworkCommonBehaviour;
 use mirror_rust::mirror::core::network_behaviour::NetworkBehaviour;
 use mirror_rust::mirror::core::network_identity::NetworkIdentity;
 use mirror_rust::mirror::core::network_reader::NetworkReader;
-use mirror_rust::mirror::core::network_server::NetworkServerStatic;
+use mirror_rust::mirror::core::network_server::{NetworkServerStatic, NETWORK_BEHAVIOURS};
 
 pub trait PlayerScript {
     fn invoke_user_code_cmd_send_player_message_string(
@@ -23,22 +24,41 @@ pub trait PlayerScript {
 
 impl PlayerScript for NetworkCommonBehaviour {
     fn invoke_user_code_cmd_send_player_message_string(
-        identity: &mut NetworkIdentity,
+        conn_id: u64,
+        net_id: u32,
         component_index: u8,
-        _func_hash: u16,
+        func_hash: u16,
         reader: &mut NetworkReader,
-        _conn_id: u64,
     ) {
         if !NetworkServerStatic::active() {
             log_error!("Command CmdClientToServerSync called on client.");
             return;
         }
-        NetworkBehaviour::early_invoke(identity, component_index)
-            .as_any_mut()
-            .downcast_mut::<Self>()
-            .unwrap()
-            .user_code_cmd_send_player_message_string(reader, _func_hash, _conn_id);
-        NetworkBehaviour::late_invoke(identity, component_index);
+
+        // 获取 NetworkBehaviour
+        match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", net_id, component_index)) {
+            TryResult::Present(mut component) => {
+                component
+                    .as_any_mut()
+                    .downcast_mut::<Self>()
+                    .unwrap()
+                    .user_code_cmd_send_player_message_string(reader, func_hash, conn_id);
+            }
+            TryResult::Absent => {
+                log_error!(
+                    "NetworkBehaviour not found by net_id: {}, component_index: {}",
+                    net_id,
+                    component_index
+                );
+            }
+            TryResult::Locked => {
+                log_error!(
+                    "NetworkBehaviour locked by net_id: {}, component_index: {}",
+                    net_id,
+                    component_index
+                );
+            }
+        }
     }
 
     fn user_code_cmd_send_player_message_string(
