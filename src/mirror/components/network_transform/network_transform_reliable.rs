77b1cdb489cf9d1dd1687e2c2dbd3d76f51616e7
aurorax-neo <15047150695@163.com>
2025-01-07 16:43:57 +0800

    fix(network): Fix command invocation and component lookup
    Fixes command invocation on server by correctly fetching
    NetworkBehaviour using net_id and component_index. Also
    changes to use try_get_mut for accessing the behaviour.

--- src/mirror/components/network_transform/network_transform_reliable.rs
+++ src/mirror/components/network_transform/network_transform_reliable.rs
@@ -5,12 +5,11 @@ use crate::mirror::components::network_transform::network_transform_base::{
 use crate::mirror::components::network_transform::transform_snapshot::TransformSnapshot;
 use crate::mirror::core::backend_data::NetworkBehaviourComponent;
 use crate::mirror::core::network_behaviour::{
-    GameObject, NetworkBehaviour, NetworkBehaviourTrait, SyncDirection, SyncMode,
+    GameObject, NetworkBehaviourTrait, SyncDirection, SyncMode,
 };
 use crate::mirror::core::network_connection::NetworkConnectionTrait;
-use crate::mirror::core::network_identity::NetworkIdentity;
 use crate::mirror::core::network_reader::{NetworkReader, NetworkReaderTrait};
-use crate::mirror::core::network_server::NetworkServerStatic;
+use crate::mirror::core::network_server::{NetworkServerStatic, NETWORK_BEHAVIOURS};
 use crate::mirror::core::network_time::NetworkTime;
 use crate::mirror::core::network_writer::{NetworkWriter, NetworkWriterTrait};
 use crate::mirror::core::network_writer_pool::NetworkWriterPool;
@@ -238,22 +237,41 @@ impl NetworkTransformReliable {
 
     // InvokeUserCode_CmdTeleport__Vector3
     fn invoke_user_code_cmd_teleport_vector3(
-        identity: &mut NetworkIdentity,
+        conn_id: u64,
+        net_id: u32,
         component_index: u8,
-        _func_hash: u16,
+        func_hash: u16,
         reader: &mut NetworkReader,
-        _conn_id: u64,
     ) {
         if !NetworkServerStatic::active() {
             log_error!("Command CmdTeleport called on client.");
             return;
         }
-        NetworkBehaviour::early_invoke(identity, component_index)
-            .as_any_mut()
-            .downcast_mut::<Self>()
-            .unwrap()
-            .user_code_cmd_teleport_vector3(reader.read_vector3());
-        NetworkBehaviour::late_invoke(identity, component_index);
+
+        // 获取 NetworkBehaviour
+        match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", net_id, component_index)) {
+            TryResult::Present(mut component) => {
+                component
+                    .as_any_mut()
+                    .downcast_mut::<Self>()
+                    .unwrap()
+                    .user_code_cmd_teleport_vector3(reader.read_vector3());
+            }
+            TryResult::Absent => {
+                log_error!(
+                    "NetworkBehaviour not found by net_id: {}, component_index: {}",
+                    net_id,
+                    component_index
+                );
+            }
+            TryResult::Locked => {
+                log_error!(
+                    "NetworkBehaviour locked by net_id: {}, component_index: {}",
+                    net_id,
+                    component_index
+                );
+            }
+        }
     }
 
     // UserCode_CmdTeleport_Vector3
@@ -285,25 +303,44 @@ impl NetworkTransformReliable {
 
     // InvokeUserCode_CmdTeleport__Vector3__Quaternion
     fn invoke_user_code_cmd_teleport_vector3_quaternion(
-        identity: &mut NetworkIdentity,
+        conn_id: u64,
+        net_id: u32,
         component_index: u8,
-        _func_hash: u16,
+        func_hash: u16,
         reader: &mut NetworkReader,
-        _conn_id: u64,
     ) {
         if !NetworkServerStatic::active() {
             log_error!("Command CmdTeleport called on client.");
             return;
         }
-        NetworkBehaviour::early_invoke(identity, component_index)
-            .as_any_mut()
-            .downcast_mut::<Self>()
-            .unwrap()
-            .user_code_cmd_teleport_vector3_quaternion(
-                reader.read_vector3(),
-                reader.read_quaternion(),
-            );
-        NetworkBehaviour::late_invoke(identity, component_index);
+
+        // 获取 NetworkBehaviour
+        match NETWORK_BEHAVIOURS.try_get_mut(&format!("{}_{}", net_id, component_index)) {
+            TryResult::Present(mut component) => {
+                component
+                    .as_any_mut()
+                    .downcast_mut::<Self>()
+                    .unwrap()
+                    .user_code_cmd_teleport_vector3_quaternion(
+                        reader.read_vector3(),
+                        reader.read_quaternion(),
+                    );
+            }
+            TryResult::Absent => {
+                log_error!(
+                    "NetworkBehaviour not found by net_id: {}, component_index: {}",
+                    net_id,
+                    component_index
+                );
+            }
+            TryResult::Locked => {
+                log_error!(
+                    "NetworkBehaviour locked by net_id: {}, component_index: {}",
+                    net_id,
+                    component_index
+                );
+            }
+        }
     }
 
     // UserCode_CmdTeleport_Vector3_Quaternion
