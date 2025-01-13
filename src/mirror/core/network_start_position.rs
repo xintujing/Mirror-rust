12cfee4ba60ee7d4f08e1fafc995f014c992ca9f
aurorax-neo <15047150695@163.com>
2025-01-13 19:12:43 +0800

    feat(mirror): Generate random start positions
    Generates 5 random start positions within a -8 to 8 range
    for x and z, y is fixed at 0.5. Uses NetworkManagerStatic
    to register them.

--- src/mirror/core/network_start_position.rs
+++ src/mirror/core/network_start_position.rs
@@ -1,10 +1,21 @@
 use crate::mirror::components::network_transform::network_transform_base::Transform;
-use crate::mirror::core::network_manager::NetworkManager;
+use crate::mirror::core::network_manager::NetworkManagerStatic;
+use nalgebra::Vector3;
+use rand::Rng;
 
 pub struct NetworkStartPosition;
 impl NetworkStartPosition {
-
-    pub fn awake(){
-        NetworkManager::register_start_position(Transform::default());
+    pub fn awake() {
+        let mut start = Transform::default();
+        // 在生成 五个随机位置
+        for _ in 0..5 {
+            let x = rand::rng().random_range(-8.0..8.0);
+            let y = 0.5;
+            let z = rand::rng().random_range(-8.0..8.0);
+            let v3 = Vector3::new(x, y, z);
+            start.position = v3;
+            start.local_position = v3;
+            NetworkManagerStatic::add_start_position(start);
+        }
     }
 }
